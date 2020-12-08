ce3732cdfbb27f7eed082be08986751edcdc5846
Hathainuch626 <Hathainuch.nh@gmail.com>
2020-12-08 13:52:32 +0700

    updateform

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,23 +6,83 @@
         <title>Laravel</title>
 
         <!-- CSS -->
-        <link rel="stylesheet" href="{{ asset('/css/app.css') }}" >
+        <link rel="stylesheet" href="{{ asset('./css/app.css') }}" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!-- jquery.Thailand.js -->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js"></script>
+        <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js"></script>
+        <link rel="stylesheet" href="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css">
+        <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
+        
         <!-- Select2 -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
         <!-- Styles -->
         <style>
             body {
-                font-family: 'Nunito';
+                font-family: 'Prompt';
+                background: #E5E5E5;
             }
+
+            .nav {
+                position: absolute;
+                width: 100%;
+                height: 64px;
+                left: 0px;
+                top: 1px;
+                background: #FFFFFF;
+                box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
+            }
+
+            img {
+                position: absolute;
+                width: 40px;
+                height: 40px;                
+                background: url(cmu_logo.png);
+            }
+
+            .position {
+                position: absolute;
+                width: 559px;
+                height: 31px;
+                left: 450px;
+                top: 30px;
+
+                font-family: Prompt;
+                font-style: normal;
+                font-weight: normal;
+                font-size: 18px;
+                line-height: 27px;
+
+                color: #363636;
+            }
+
             .col {
                 width: 100%;
             }
+
+            .province {
+                display: block;
+                width: 100%;
+                height: calc(1.5em + .75rem + 2px);
+                padding: .375rem .75rem;
+                font-size: 1rem;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #495057;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #ced4da;
+                border-radius: .25rem;
+                transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+            }
+
             .select {
                 margin: auto;
             }
@@ -38,18 +98,51 @@
                 height: 38px !important;
                 vertical-align: middle;
             }
+
+            .form-control {
+                display: block;
+                width: 100%;
+                height: calc(1.5em + .75rem + 2px);
+                padding: .375rem .75rem;
+                font-size: 1rem;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #495057;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #ced4da;
+                border-radius: .25rem;
+                transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+            }
+
+            .btn {
+                position: absolute;
+                width: 137px;
+                height: 48px;
+                left: 652px;
+                top: 850px;
+                color: #FFFFFF;
+                align-items: center;
+                text-align: center;
+
+                /* #C8C7CC */
+
+                background: #C8C7CC;
+                box-shadow: inset 0px -2px 0px rgba(0, 0, 0, 0.2);
+            }
         </style>
-        <script>
-            $(document).ready(function() {
-                $('.form-control-js').select2()
-            });
-        </script>
     </head>
     <body >
+        <ul class="nav justify-content-center">
+            <li class="nav-item justify-content-center">
+                <img src="cmu_logo.png" width="30" height="30" alt="">
+                <center><h3 class="position">ศูนย์บริหารจัดการเมืองอัจฉริยะ มหาวิทยาลัยเชียงใหม่</h3></center>
+            </li>
+        </ul>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-            <div class="card" style="margin: 30px 15px 10px">
+            <div class="card" style="margin: 100px 15px 10px">
                     <div class="card-header" >{{ __('รหัสขอออกใบกำกับภาษีของคุณ') }} </div>
                     <div class="card-body">
                             <div class="form-group row">
@@ -91,106 +184,39 @@
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="inputprovince" class="col-md-6 col-form-label text-md-left" >{{ __('จังหวัด') }}</label>
+                                <label for="province" class="col-md-6 col-form-label text-md-left" >{{ __('จังหวัด') }}</label>
                                 <div class="col-md-6" style="width: 100%">
-                                    <select class="form-control-js" name="inputprovince" style="width: 100%; height: 38px;">
-                                        <option >เลือกจังหวัด</option>
-                                        <option value="กร">กรุงเทพมหานคร</option>
-                                        <option value="กำ">กำแพงเพชร </option>
-                                        <option value="ชัยน">ชัยนาท</option>
-                                        <option value="นครน">นครนายก</option>
-                                        <option value="นครป">นครปฐม</option>
-                                        <option value="นครส">นครสวรรค์ </option>
-                                        <option value="นน">นนทบุรี </option>
-                                        <option value="ปทุ">ปทุมธานี</option>
-                                        <option value="พระ">พระนครศรีอยุธยา </option>
-                                        <option value="พิจิ">พิจิตร </option>
-                                        <option value="พิษ">พิษณุโลก </option>
-                                        <option value="เพชรบู">เพชรบูรณ์</option>
-                                        <option value="ลพ">ลพบุรี</option>
-                                        <option value="สมุทรป">สมุทรปราการ</option>
-                                        <option value="สมุทรสง">สมุทรสงคราม</option>
-                                        <option value="สมุทรสา">สมุทรสาคร </option>
-                                        <option value="สิง">สิงห์บุรี </option>
-                                        <option value="สุโข">สุโขทัย</option>
-                                        <option value="สุพรร">สุพรรณบุรี</option>
-                                        <option value="สระบ">สระบุรี </option>
-                                        <option value="อ่าง">อ่างทอง</option>
-                                        <option value="อุท">อุทัยธานี</option>
-                                        <option value="จัน">จันทบุรี</option>
-                                        <option value="ฉะ">ฉะเชิงเทรา</option>
-                                        <option value="ชล">ชลบุรี</option>
-                                        <option value="ตรา">ตราด</option>
-                                        <option value="ปรา">ปราจีนบุรี</option>
-                                        <option value="ระย">ระยอง</option>
-                                        <option value="สระแ">สระแก้ว</option>
-                                        <option value="กาญ">กาญจนบุรี</option>
-                                        <option value="ตา">ตาก</option>
-                                        <option value="ประ">ประจวบคีรีขันธ์</option>
-                                        <option value="เพชรบุ">เพชรบุรี</option>
-                                        <option value="ราชบ">ราชบุรี</option>
-                                        <option value="อุต">อุตรดิตถ์</option>
-                                        <option value="ลำพ">ลำพูน</option>
-                                        <option value="ลำป">ลำปาง </option>
-                                        <option value="แม่ฮ่อ">แม่ฮ่องสอน</option>
-                                        <option value="เชียงร">เชียงราย</option>
-                                        <option value="เชียงใ">เชียงใหม่</option>
-                                        <option value="น่า">น่าน</option>
-                                        <option value="พะ">พะเยา</option>
-                                        <option value="แพ">แพร่</option>
-                                        <option value="กาฬ">กาฬสินธุ์</option>
-                                        <option value="ขอ">ขอนแก่น</option>
-                                        <option value="ชัยภู">ชัยภูมิ</option>
-                                        <option value="นครพ">นครพนม</option>
-                                        <option value="นคราช">นครราชสีมา</option>
-                                        <option value="บึง">บึงกาฬ</option>
-                                        <option value="บุร">บุรีรัมย์ </option>
-                                        <option value="มหาส">มหาสารคาม</option>
-                                        <option value="มุก">มุกดาหาร</option>
-                                        <option value="ยโ">ยโสธร </option>
-                                        <option value="ร้อ">ร้อยเอ็ด</option>
-                                        <option value="เล">เลย</option>
-                                        <option value="สก">สกลนคร</option>
-                                        <option value="สุริ">สุรินทร์</option>
-                                        <option value="ศรี">ศรีสะเกษ</option>
-                                        <option value="หนองค">หนองคาย</option>
-                                        <option value="หนองบ">หนองบัวลำภู</option>
-                                        <option value="อุด">อุดรธานี</option>
-                                        <option value="อุบ">อุบลราชธานี </option>
-                                        <option value="อำ">อำนาจเจริญ</option>
-                                        <option value="กระ">กระบี่</option>
-                                        <option value="ชุม">ชุมพร</option>
-                                        <option value="ตรั">ตรัง</option>
-                                        <option value="นครศ">นครศรีธรรมราช</option>
-                                        <option value="นรา">นราธิวาส</option>
-                                        <option value="ปัต">ปัตตานี</option>
-                                        <option value="พัง">พังงา</option>
-                                        <option value="พัท">พัทลุง </option>
-                                        <option value="ภู">ภูเก็ต</option>
-                                        <option value="ระน">ระนอง</option>
-                                        <option value="สต">สตูล</option>
-                                        <option value="สงข">สงขลา</option>
-                                        <option value="สุราษ">สุราษฎร์ธานี</option>
-                                        <option value="ยะ">ยะลา</option>
-                                    </select>
+                                    <input type="text" name="province" id="province" class="form-control">
+                                    {{-- <select class="form-control-js" name="province" id="province" style="width: 100%; height: 38px;">
+                                       
+                                    </select> --}}
+                                    {{-- <input type="text" name="province" id="province"> --}}
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="inputcounty" class="col-md-6 col-form-label text-md-left">{{ __('เขต/อำเภอ') }}</label>
-                                <div class="col-md-6" style="margin: auto;">
-                                    <input id="inputcounty" type="text" class="form-control" name="county">
+                                <label for="amphoe" class="col-md-6 col-form-label text-md-left" >{{ __('เขต/อำเภอ') }}</label>
+                                <div class="col-md-6" style="width: 100%">
+                                    <input type="text" id="amphoe" class="form-control">
+                                    {{-- <select class="form-control-js" name="amphoe" id="amphoe" style="width: 100%; height: 38px;">
+                                        
+                                    </select> --}}
+                                    {{-- <input type="text" id="amphoe"> --}}
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="inputdistrict" class="col-md-6 col-form-label text-md-left">{{ __('ตำบล') }}</label>
-                                <div class="col-md-6" style="margin: auto;">
-                                    <input id="inputdistrict" type="text" class="form-control" name="district" >
+                                <label for="district" class="col-md-6 col-form-label text-md-left" >{{ __('ตำบล') }}</label>
+                                <div class="col-md-6" style="width: 100%">
+                                    <input type="text" id="district" class="form-control">
+                                    {{-- <select class="form-control-js" name="district" id="district" style="width: 100%; height: 38px;">
+                                        
+                                    </select> --}}
+                                    {{-- <input type="text" id="district"> --}}
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="inputpostalcode" class="col-md-6 col-form-label text-md-left">{{ __('รหัสไปรษณีย์') }}</label>
+                                <label for="zipcode" class="col-md-6 col-form-label text-md-left">{{ __('รหัสไปรษณีย์') }}</label>
                                 <div class="col-md-6" style="margin: auto;">
-                                    <input id="inputpostalcode" type="text" class="form-control" name="postalcode" >
+                                    <input id="zipcode" type="text" class="form-control" name="zipcode" >
                                 </div>
                             </div>
                         </form>
@@ -198,7 +224,21 @@
                 </div>
             </div>
         </div>
-        <center><button type="submit" value="submit" class="btn btn-primary" style="margin: auto;">ยืนยัน</button></center>
+        <button type="submit" value="submit" class="btn btn" >ยืนยัน</button>
     </div>
     </body>
 </html>
+ <script>
+
+            $(document).ready(function() {
+                $('.form-control-js').select2()
+            });
+
+            // เรียกใช้ jquery.Thailand.js
+            $.Thailand({ 
+                $district: $('#district'), // ตำบล
+                $amphoe: $('#amphoe'), // อำเภอ
+                $province: $('#province'), // จังหวัด
+                $zipcode: $('#zipcode'), // รหัสไปรษณีย์
+            });
+</script>
